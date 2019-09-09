649355e565de088751c029fd8f0884184b065f1f
Leonidas Yopan <leonidasyopan@gmail.com>
2019-09-09 19:07:17 -0300

    added the images and bcground colors

--- exercisios/hora/js/applications.js
+++ exercisios/hora/js/applications.js
@@ -1,1 +1,31 @@
-sadfas
+function carregar() {
+    var mensagem = window.document.getElementById('mensagem');
+    var imagem = window.document.getElementById('imagem');
+
+    var data = new Date();
+    // var hora = data.getHours();
+
+    var hora = 21;
+
+    mensagem.innerHTML = `Agora são ${hora} horas.`;
+
+    if (hora >= 0 && hora < 12 ) {
+        // BOM DIA 
+        imagem.src = 'img/morning.png';
+        document.body.style.background = '#FF9901';
+    }   else if (hora >= 12 && hora < 18) {
+        // BOA TARDE
+        imagem.src = 'img/afternoon.png';
+        document.body.style.background = '#8EA6BD';
+    }   else if (hora >= 18 && hora < 20) {
+        // GOOD EVENING
+        imagem.src = 'img/evening.png';
+        document.body.style.background = '#B4B6BD';
+    } else {
+        // GOOD NIGHT
+        imagem.src = 'img/night.png';
+        document.body.style.background = '#19374F';
+    }
+}
+
+
