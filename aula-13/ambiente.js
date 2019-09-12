53459880c27fd34890f4f83b0181dfba8d1e6557
Leonidas Yopan <leonidasyopan@gmail.com>
2019-09-12 18:35:37 -0300

    Completada a aula 13

---
+++ aula-13/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1;
+
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 6);
+
+
+
+/*
+While sample:
+
+var c = 1;
+
+while (c <= 500) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+
+*/
